723a49a0b0cc4a726ec91a6f6c86a1515c5fbed1
unknown <obirikan020@gmail.com>
2022-07-08 11:39:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+     {/* <Navbar/> */}
+     <nav className='flex justify-between items-center shadow-md h-16 text-black relative'>
+    <div className='pl-5'>
+        logo
     </div>
+     <div className='flex justify-between items-center m-auto px-45 pr-5 md:flex hidden'>
+        <p className='pr-7'>home</p>
+        <p className='pr-7'>Event</p>
+        <p className='pr-7'>Family</p>
+        <p className='pr-7'>Who we are</p>
+        <p className='pr-7'>Contact</p>
+     </div>
+      <div className='pr-5 md:hidden'>
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+      </svg>
+      </div>
+</nav>
+<div className='
+  flex justify-center items-center h-screen text-5xl'
+  bg-gradient-to-r from-cyan-500 to-blue-500
+  >
+  LandingPage
+  </div>
+     {/* <Routes>
+      <Route path='/' element={<LandingPage/>}/>
+     </Routes> */}
+     
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../index.css'
+const LandingPage = () => {
+  return (
+    <div className='
+    flex justify-center items-center h-screen text-5xl 
+    
+    bg-red-500
+    '>
+      <h1>Welcome To C C C C</h1>
+    </div>
+  )
+}
+
+export default LandingPage
