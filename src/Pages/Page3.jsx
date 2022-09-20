9cd15d513ad31fdd73f1b1590f2a9cd07e34856b
unknown <obirikan020@gmail.com>
2022-09-20 16:01:20 +0000

    *

--- src/Pages/Page3.jsx
+++ src/Pages/Page3.jsx
@@ -4,7 +4,7 @@ const Page3 = () => {
   return (
     <div className='appre'>
         <div className='appre-child'>
-            <h3>we are glad you are here</h3>
+             <h1 className='appre-text'>we are glad you are here</h1>
             <button>Get to know us better</button>
         </div>
     </div>
