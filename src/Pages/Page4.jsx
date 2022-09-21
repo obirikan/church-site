421a9f1b5da4984048a4774107933dc67d1918e9
unknown <obirikan020@gmail.com>
2022-09-21 13:24:04 +0000

    *

--- src/Pages/Page4.jsx
+++ src/Pages/Page4.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import i from '../assets/1.jpg'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+var Carousel = require('react-responsive-carousel').Carousel;
+
 
 const Page4 = () => {
   return (
+    <Carousel showArrows={true} >
     <div>
-       <img src={i} alt="" />
+        <img src={i} alt='nothin' />
+        <p className="legend">Legend 1</p>
     </div>
+    <div>
+       <img src={i} alt='nothin' />   
+        <p className="legend">Legend 2</p>
+    </div>
+    <div>
+    <img src={i} alt='nothin' />
+        <p className="legend">Legend 3</p>
+    </div>
+    <div>
+    <img src={i} alt='nothin' />
+        <p className="legend">Legend 4</p>
+    </div>
+    <div>
+    <img src={i} alt='nothin' />
+        <p className="legend">Legend 5</p>
+    </div>
+    <div>
+    <img src={i} alt='nothin' />
+        <p className="legend">Legend 6</p>
+    </div>
+</Carousel>
   )
 }
 
